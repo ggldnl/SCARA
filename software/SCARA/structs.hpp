52ec75f9bff9e42e578ddf1c690c6a6a4884f9be
Re-l <danielgigliotti99.dg@gmail.com>
2024-09-29 17:26:33 +0200

    Added toString to all the structs

--- software/SCARA/structs.hpp
+++ software/SCARA/structs.hpp
@@ -10,6 +10,10 @@ struct Point {
 
     // Constructor to initialize the Point
     Point(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+
+    String toString() const {
+        return String("Point(") + x + ", " + y + ", " + z + ")";
+    }
 };
 
 // Structure to hold the inverse kinematics solution (angles for each joint)
@@ -21,6 +25,10 @@ struct IKSolution {
 
     // Constructor to initialize the Point
     IKSolution(float _q1, float _q2, float _q3) : q1(_q1), q2(_q2), q3(_q3) {}
+
+    String toString() const {
+        return String("IKSolution(") + q1 + ", " + q2 + ", " + q3 + ")";
+    }
 };
 
 // Structure to hold the steps for each motor
@@ -37,6 +45,10 @@ struct Steps {
 
     // Constructor to initialize the Point
     Steps(long _s1, long _s2, long _s3) : s1(_s1), s2(_s2), s3(_s3) {}
+
+    String toString() const {
+        return String("Steps(") + s1 + ", " + s2 + ", " + s3 + ")";
+    }
 
 };
 
